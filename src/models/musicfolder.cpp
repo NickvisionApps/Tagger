e2c15afe86ab12d21c1d481678f25716960015ad
Nick Logozzo <nlogozzo225@gmail.com>
2022-09-12 20:07:01 -0400

    Implemented MusicFolder

---
+++ src/models/musicfolder.cpp
@@ -0,0 +1,84 @@
+#include "musicfolder.hpp"
+#include <algorithm>
+
+using namespace NickvisionTagger::Models;
+
+MusicFolder::MusicFolder() : m_parentPath{ "" }, m_includeSubfolders{ true }
+{
+
+}
+
+const std::filesystem::path& MusicFolder::getParentPath() const
+{
+    return m_parentPath;
+}
+
+void MusicFolder::setParentPath(const std::filesystem::path& parentPath)
+{
+    m_parentPath = parentPath;
+}
+
+bool MusicFolder::getIncludeSubfolders() const
+{
+    return m_includeSubfolders;
+}
+
+void MusicFolder::setIncludeSubfolders(bool includeSubfolders)
+{
+    m_includeSubfolders = includeSubfolders;
+}
+
+std::vector<std::filesystem::path> MusicFolder::getFolderPaths() const
+{
+    std::vector<std::filesystem::path> paths;
+    paths.push_back(m_parentPath);
+    if (m_includeSubfolders)
+    {
+        for (const std::filesystem::directory_entry& path : std::filesystem::recursive_directory_iterator(m_parentPath))
+        {
+            if (path.is_directory())
+            {
+                paths.push_back(path);
+            }
+        }
+    }
+    return paths;
+}
+
+const std::vector<std::shared_ptr<MusicFile>>& MusicFolder::getMusicFiles() const
+{
+    return m_files;
+}
+
+void MusicFolder::reloadMusicFiles()
+{
+    m_files.clear();
+    if (std::filesystem::exists(m_parentPath))
+    {
+        std::vector<std::string> supportedDotFileExtensions{ ".mp3", ".ogg", ".opus", ".flac", ".wma", ".wav" };
+        if (m_includeSubfolders)
+        {
+            for (const std::filesystem::path& path : std::filesystem::recursive_directory_iterator(m_parentPath))
+            {
+                if(std::find(supportedDotFileExtensions.begin(), supportedDotFileExtensions.end(), path.extension()) != supportedDotFileExtensions.end())
+                {
+                    m_files.push_back(std::make_shared<MusicFile>(path));
+                }
+            }
+        }
+        else
+        {
+            for (const std::filesystem::path& path : std::filesystem::directory_iterator(m_parentPath))
+            {
+                if(std::find(supportedDotFileExtensions.begin(), supportedDotFileExtensions.end(), path.extension()) != supportedDotFileExtensions.end())
+                {
+                    m_files.push_back(std::make_shared<MusicFile>(path));
+                }
+            }
+        }
+        std::sort(m_files.begin(), m_files.end(), [](const std::shared_ptr<MusicFile>& a, const std::shared_ptr<MusicFile>& b)
+        {
+            return *a < *b;
+        });
+    }
+}
