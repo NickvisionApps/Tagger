d0625158d7f75e9296f6ef2a8602b1b93cca2481
Nick Logozzo <nlogozzo225@gmail.com>
2022-10-15 19:46:46 -0400

    Updated changelog

--- src/ui/application.cpp
+++ src/ui/application.cpp
@@ -14,7 +14,7 @@ Application::Application(const std::string& id, GApplicationFlags flags) : m_adw
     m_appInfo.setShortName("Tagger");
     m_appInfo.setDescription("An easy-to-use music tag (metadata) editor.");
     m_appInfo.setVersion("2022.10.4-next");
-    m_appInfo.setChangelog("<ul><li></li></ul>");
+    m_appInfo.setChangelog("<ul><li>Added 'Discard Unapplied Changes' action</li><li>Improved closing and reloading dialogs</li></ul>");
     m_appInfo.setGitHubRepo("https://github.com/nlogozzo/NickvisionTagger");
     m_appInfo.setIssueTracker("https://github.com/nlogozzo/NickvisionTagger/issues/new");
     m_appInfo.setSupportUrl("https://github.com/nlogozzo/NickvisionTagger/discussions");
