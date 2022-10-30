45fbe1bcef947c497820f907e83c7a0a99ab3dfe
Nick Logozzo <nlogozzo225@gmail.com>
2022-10-30 12:42:56 -0400

    V2022.10.6

--- src/ui/application.cpp
+++ src/ui/application.cpp
@@ -13,7 +13,7 @@ Application::Application(const std::string& id, GApplicationFlags flags) : m_adw
     m_appInfo.setName("NickvisionTagger");
     m_appInfo.setShortName("Tagger");
     m_appInfo.setDescription("An easy-to-use music tag (metadata) editor.");
-    m_appInfo.setVersion("2022.10.6-next");
+    m_appInfo.setVersion("2022.10.6");
     m_appInfo.setChangelog("<ul><li>Improved appearance of waiting to be applied changes</li></ul>");
     m_appInfo.setGitHubRepo("https://github.com/nlogozzo/NickvisionTagger");
     m_appInfo.setIssueTracker("https://github.com/nlogozzo/NickvisionTagger/issues/new");
