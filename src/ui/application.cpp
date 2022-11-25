e971dc8e247f7c9cc9b108f2d8a94efc4e112f12
Nick Logozzo <nlogozzo225@gmail.com>
2022-11-25 00:45:06 -0500

    V2022.11.2

--- src/ui/application.cpp
+++ src/ui/application.cpp
@@ -14,8 +14,8 @@ Application::Application(const std::string& id, GApplicationFlags flags) : m_adw
     m_appInfo.setName("NickvisionTagger");
     m_appInfo.setShortName("Tagger");
     m_appInfo.setDescription(_("An easy-to-use music tag (metadata) editor."));
-    m_appInfo.setVersion("2022.11.1");
-    m_appInfo.setChangelog("<ul><li>Added support for m4b extension</li></ul>");
+    m_appInfo.setVersion("2022.11.2");
+    m_appInfo.setChangelog("<ul><li>Tagger will now properly set an album art's mime type to be properly displayed in some music players</li><li>Changed 'Delete Tags' shortcut to Shift+Delete to allow for Delete button to work in entry widgets</li><li>Added Croatian translation (Thanks @milotype!)</li></ul>");
     m_appInfo.setGitHubRepo("https://github.com/nlogozzo/NickvisionTagger");
     m_appInfo.setIssueTracker("https://github.com/nlogozzo/NickvisionTagger/issues/new");
     m_appInfo.setSupportUrl("https://github.com/nlogozzo/NickvisionTagger/discussions");
