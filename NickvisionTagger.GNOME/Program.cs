f0073e89e526da0cea90e8931add52a2c57b5c01
Nick Logozzo <nlogozzo225@gmail.com>
2023-08-14 15:06:20 -0400

    All - Update Changelog

--- NickvisionTagger.GNOME/Program.cs
+++ NickvisionTagger.GNOME/Program.cs
@@ -34,7 +34,8 @@ public partial class Program
         _mainWindow = null;
         _mainWindowController = new MainWindowController();
         _mainWindowController.AppInfo.Changelog =
-            @"* Lyrics stored in LRC format will now be displayed correctly as synchronized lyrics
+            @"* Added offset field to synchronized lyrics page 
+              * Lyrics stored in LRC format will now be displayed correctly as synchronized lyrics
               * Updated translations (Thanks everyone on Weblate!)";
         _application.OnActivate += OnActivate;
         if (File.Exists(Path.GetFullPath(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)) + "/org.nickvision.tagger.gresource"))
