b042f9a878c2611f4021eaf5b2a8741bcc03de31
Nick Logozzo <nlogozzo225@gmail.com>
2023-08-26 22:07:48 -0400

    Start Next Ver

--- NickvisionTagger.GNOME/Program.cs
+++ NickvisionTagger.GNOME/Program.cs
@@ -34,16 +34,7 @@ public partial class Program
         _mainWindow = null;
         _mainWindowController = new MainWindowController(args);
         _mainWindowController.AppInfo.Changelog =
-            @"* Added offset field to synchronized lyrics page 
-              * Added the ability to import and export LRC files in the synchronized lyrics page
-              * Added the ability to ignore a part of a file name in a format string by specifying an empty `%%`
-              * Fixed an issue where synchronized lyrics stored in the LRC format were not displayed correctly
-              * Fixed an issue where using WebP image for album art caused a crash
-              * Fixed an issue where album art would not show in Spotify
-              * Fixed an issue where drag and drop would cause a crash
-              * Improved speed of loading and saving files
-              * Improved fingerprint calculation and process management
-              * Updated translations (Thanks everyone on Weblate!)";
+            @"* Updated translations (Thanks everyone on Weblate!)";
         _application.OnActivate += OnActivate;
         if (File.Exists(Path.GetFullPath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)) + "/org.nickvision.tagger.gresource"))
         {
