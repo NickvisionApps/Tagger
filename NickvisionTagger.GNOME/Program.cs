df6d794f4e4bc1ba1044c27fa96af5e37f2a1229
Nick Logozzo <nlogozzo225@gmail.com>
2023-08-27 14:42:09 -0400

    Update Changelog

--- NickvisionTagger.GNOME/Program.cs
+++ NickvisionTagger.GNOME/Program.cs
@@ -34,7 +34,8 @@ public partial class Program
         _mainWindow = null;
         _mainWindowController = new MainWindowController(args);
         _mainWindowController.AppInfo.Changelog =
-            @"* Updated translations (Thanks everyone on Weblate!)";
+            @"* Added a new web service to download lyrics for a song
+              * Updated translations (Thanks everyone on Weblate!)";
         _application.OnActivate += OnActivate;
         if (File.Exists(Path.GetFullPath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)) + "/org.nickvision.tagger.gresource"))
         {
