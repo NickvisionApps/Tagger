23a9cbcd7f1ca1e1a13d55cda758d17a613d8b27
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-09-11 04:10:17 +0300

    All - Update Aura to 2023.9.3

--- NickvisionTagger.Shared/Controllers/MainWindowController.cs
+++ NickvisionTagger.Shared/Controllers/MainWindowController.cs
@@ -141,7 +141,7 @@ public class MainWindowController : IDisposable
                 _folderToLaunch = dir;
             }
         }
-        Aura.Init("org.nickvision.tagger", "Nickvision Tagger", _("Tagger"), _("Tag your music"));
+        Aura.Init("org.nickvision.tagger", "Nickvision Tagger");
         if (Directory.Exists($"{UserDirectories.Config}{Path.DirectorySeparatorChar}Nickvision{Path.DirectorySeparatorChar}{AppInfo.Name}"))
         {
             // Move config files from older versions and delete old directory
@@ -158,6 +158,8 @@ public class MainWindowController : IDisposable
         Aura.Active.SetConfig<Configuration>("config");
         Configuration.Current.Saved += ConfigurationSaved;
         AppInfo.Version = "2023.9.1-next";
+        AppInfo.ShortName = _("Tagger");
+        AppInfo.Description = _("Tag your music");
         AppInfo.SourceRepo = new Uri("https://github.com/NickvisionApps/Tagger");
         AppInfo.IssueTracker = new Uri("https://github.com/NickvisionApps/Tagger/issues/new");
         AppInfo.SupportUrl = new Uri("https://github.com/NickvisionApps/Tagger/discussions");
