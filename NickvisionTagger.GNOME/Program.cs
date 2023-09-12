6f7fbe3a3c8843d42ec1f97a34da8010d0888e27
Nick Logozzo <nlogozzo225@gmail.com>
2023-09-12 09:00:35 -0400

    All - Update Changelog

--- NickvisionTagger.GNOME/Program.cs
+++ NickvisionTagger.GNOME/Program.cs
@@ -35,7 +35,9 @@ public partial class Program
         _mainWindowController = new MainWindowController(args);
         _mainWindowController.AppInfo.Changelog =
             @"* Added the ability to create playlists within a music folder
+              * Synchronized lyric timestamps will now be shown in the mm:ss.xx format as per the LRC specification. When creating a new lryic, both hh:mm:ss and mm:ss.xx can be specified and Tagger will convert them appropriately
               * Tagger will display headers in the list of music files when sorting to provide a more organized view of files
+              * Fixes an issue where lrc files were not importing correctly
               * Updated translations (Thanks everyone on Weblate!)";
         _application.OnActivate += OnActivate;
         if (File.Exists(Path.GetFullPath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)) + "/org.nickvision.tagger.gresource"))
