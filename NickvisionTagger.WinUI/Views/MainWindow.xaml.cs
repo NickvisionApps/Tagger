1463ea686cf435e439c41730d3116238dcf2bbfb
Nick Logozzo <nlogozzo225@gmail.com>
2023-09-30 21:00:25 -0400

    WinUI - CommandBar Buttons

--- NickvisionTagger.WinUI/Views/MainWindow.xaml.cs
+++ NickvisionTagger.WinUI/Views/MainWindow.xaml.cs
@@ -172,6 +172,10 @@ public sealed partial class MainWindow : Window
         HomeDiscussionsButtonLabel.Text = _("Discussions");
         StatusPageNoFiles.Title = _("No Music Files Found");
         StatusPageNoFiles.Description = _("Try a different library");
+        CmdReloadLibrary.Label = _("Reload Library");
+        CmdSaveTag.Label = _("Save Tag");
+        CmdDeleteTag.Label = _("Delete Tag");
+        CmdDownloadMusicBrainz.Label = _("Download MusicBrainz Metadata");
         SearchMusicFiles.PlaceholderText = _("Search for filename (type ! for advanced search)...");
         ToolTipService.SetToolTip(BtnAdvancedSearchInfo, _("Advanced Search Info"));
         StatusPageNoSelected.Title = _("No Selected Music Files");
@@ -183,6 +187,7 @@ public sealed partial class MainWindow : Window
         TxtFilename.Header = _("File Name");
         TxtFilename.PlaceholderText = _("Enter file name here");
         LblMainProperties.Text = _("Main Properties");
+        LblBtnManageLyrics.Text = _("Manage Lyrics");
         TxtTitle.Header = _("Title");
         TxtTitle.PlaceholderText = _("Enter title here");
         TxtArtist.Header = _("Artist");
@@ -912,6 +917,7 @@ public sealed partial class MainWindow : Window
             MenuAddToPlaylist.IsEnabled = false;
             MenuRemoveFromPlaylist.IsEnabled = false;
             MenuTag.IsEnabled = false;
+            CmdTag.Visibility = Visibility.Collapsed;
             ViewStack.CurrentPageName = "Home";
             StatusBar.Visibility = Visibility.Collapsed;
         }
@@ -941,7 +947,9 @@ public sealed partial class MainWindow : Window
         //Update Properties
         SelectedViewStack.CurrentPageName = _controller.SelectedMusicFiles.Count > 0 ? "Selected" : "NoSelected";
         MenuTag.IsEnabled = _controller.SelectedMusicFiles.Count > 0;
+        CmdTag.Visibility = _controller.SelectedMusicFiles.Count > 0 ? Visibility.Visible : Visibility.Collapsed;
         MenuManageLyrics.IsEnabled = _controller.SelectedMusicFiles.Count == 1;
+        BtnManageLyrics.IsEnabled = _controller.SelectedMusicFiles.Count == 1;
         MenuCustomProperties.IsEnabled = _controller.SelectedMusicFiles.Count == 1;
         CustomPropertiesHeader.Visibility = _controller.SelectedMusicFiles.Count == 1 ? Visibility.Visible : Visibility.Collapsed;
         ListCustomProperties.Visibility = _controller.SelectedMusicFiles.Count == 1 ? Visibility.Visible : Visibility.Collapsed;
