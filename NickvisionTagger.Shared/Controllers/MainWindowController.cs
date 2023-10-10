4df6bb99f83d1b59bda5363f8be27b8df5c1c460
Nick Logozzo <nlogozzo225@gmail.com>
2023-10-10 11:05:24 -0400

    V2023.10.0-beta1

--- NickvisionTagger.Shared/Controllers/MainWindowController.cs
+++ NickvisionTagger.Shared/Controllers/MainWindowController.cs
@@ -163,7 +163,7 @@ public class MainWindowController : IDisposable
         }
         Aura.Active.SetConfig<Configuration>("config");
         Configuration.Current.Saved += ConfigurationSaved;
-        AppInfo.Version = "2023.10.0-next";
+        AppInfo.Version = "2023.10.0-beta1";
         AppInfo.ShortName = _("Tagger");
         AppInfo.Description = _("Tag your music");
         AppInfo.SourceRepo = new Uri("https://github.com/NickvisionApps/Tagger");
